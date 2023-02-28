32681eff29c26017d9fa21e01d28c31b412303d0
DaviSonda <126525330+DaviSonda@users.noreply.github.com>
2023-02-27 22:11:13 -0300

    Add files via upload

---
+++ Aula1/Produto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1
+{
+    public class Produto
+    {
+        private int codigo;
+        private string descricao;
+        private double valor;
+
+        public Produto()
+        {
+            this.codigo = 0;
+            this.descricao = "";
+            this.valor = 0;
+        }
+        public Produto(int codigo, string descricao, double valor)
+        {
+            this.codigo = codigo;
+            this.descricao = descricao;
+            this.valor = valor;
+        }
+
+        public int Codigo { get { return codigo; } set { codigo = value; } }
+        public string Descricao { get { return descricao; } set { descricao = value; } }
+        public double Valor { get { return valor; } set { valor = value; } }
+
+        public double Reajuste(double valor)
+        {
+            this.valor = valor;
+            return valor;
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return descricao;
+        }
+    }
+}
---
+++ Aula1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto prod = new Produto(1, "teste", 7.5);
+            Console.WriteLine("\nPré Reajuste: " + prod.Valor);
+            prod.Reajuste(40);
+            Console.WriteLine("\nPós Reajuste: " + prod.Valor);
+        }
+    }
+}
