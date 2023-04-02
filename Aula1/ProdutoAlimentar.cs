5b8b950c547293c68ff019d0531b5a7a907f1be6
Carlos Eduardo <62404941+cecgil@users.noreply.github.com>
2023-04-02 17:36:30 -0300

    Add files via upload
    Commit class ProdutoDigital e ProdutoAlimentar

---
+++ Aula1/ProdutoAlimentar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1
+{
+    public class ProdutoAlimentar : Produto
+    {
+        private int dataFabricacao;
+        private int dataValidade;
+
+        public ProdutoAlimentar(int dataFabricacao, int dataValidade)
+        {
+            this.dataFabricacao = dataFabricacao;
+            this.dataValidade = dataValidade;
+        }
+
+        public int DataFabricacao { get { return dataFabricacao; } set { dataFabricacao = value; } }
+
+        public int DataValidade { get { return dataValidade; } set { dataFabricacao = value; } }
+
+        public override string ToString()
+        {
+            return "\nData de fabricação: " + dataFabricacao + "\nData de validade: " + dataValidade;
+        }
+    }
+}
---
+++ Aula1/ProdutoDigital.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1
+{
+    public class ProdutoDigital : Produto
+    {
+        private int tamanhoArquivo;
+        private int versaoProduto;
+
+        public ProdutoDigital(int tamanhoArquivo, int versaoProduto)
+        {
+            this.tamanhoArquivo = tamanhoArquivo;
+            this.versaoProduto = versaoProduto;
+
+        }
+
+        public int TamanhoArquivo { get {  return tamanhoArquivo; } set {  tamanhoArquivo = value; } }
+        public int VersaoProduto { get { return versaoProduto; } set {  versaoProduto = value; } }
+
+        public override string ToString()
+        {
+            return "\nTamanho do arquivo: " + tamanhoArquivo + "\nVersão do Produto: " + versaoProduto;
+        }
+    }
+}
