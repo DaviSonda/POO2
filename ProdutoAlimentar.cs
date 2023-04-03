03c94094c9821f5ca28e8926af8957e3d29f37f6
Carlos Eduardo <62404941+cecgil@users.noreply.github.com>
2023-04-03 20:37:05 -0300

    Add files via upload
    Correção classes PD e PA

---
+++ ProdutoAlimentar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1
+{
+    public class ProdutoAlimentar : Produto
+    {
+        private int dataFabricacao;
+        private int dataValidade;
+
+        public ProdutoAlimentar() : base()
+        {
+            dataFabricacao = 0;
+            dataValidade = 0;
+        }
+
+        public ProdutoAlimentar(int dataFabricacao, int dataValidade) : base()
+       
+        {
+            this.dataFabricacao = dataFabricacao;
+            this.dataValidade = dataValidade;
+        }
+
+        public int DataFabricacao { get { return dataFabricacao; } set { dataFabricacao = value; } }
+
+        public int DataValidade { get { return dataValidade; } set { dataFabricacao = value; } }
+
+        public override string ToString()
+        {
+
+            return "\nCódigo: " + Codigo + "\nDescricao: " + Descricao + "\nValor: " + Valor + "\nData de fabricação: " + dataFabricacao + "\nData de validade: " + dataValidade;
+        }
+    }
+}
---
+++ ProdutoDigital.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1
+{
+    public class ProdutoDigital : Produto
+    {
+        private int tamanhoArquivo;
+        private int versaoProduto;
+
+        public ProdutoDigital() : base()
+        {
+            tamanhoArquivo = 0;
+            versaoProduto = 0;
+        }
+
+        public ProdutoDigital(int tamanhoArquivo, int versaoProduto) : base()
+        {
+            this.tamanhoArquivo = tamanhoArquivo;
+            this.versaoProduto = versaoProduto;
+
+        }
+
+        public int TamanhoArquivo { get {  return tamanhoArquivo; } set {  tamanhoArquivo = value; } }
+        public int VersaoProduto { get { return versaoProduto; } set {  versaoProduto = value; } }
+
+        public override string ToString()
+        {
+            return "\nCódigo: " + Codigo + "\nDescricao: " + Descricao + "\nValor: " + Valor + "\nTamanho do arquivo: " + tamanhoArquivo + "\nVersão do Produto: " + versaoProduto;
+            
+        }
+    }
+}
