c8f5ec3a555cf3d643cdd05888bf3e7677d76e7b
DaviSSonego <113932869+DaviSSonego@users.noreply.github.com>
2023-04-17 20:08:41 -0300

    Ajustes

--- Aula1/Menu.cs
+++ Aula1/Menu.cs
@@ -9,264 +9,74 @@ namespace Aula1
     public class Menu
     {
         EntradaDados entrada;
-        Cadastro cad;
-        double media;
-        Pedido pedido;
-        ItemPedido itemPedido;
         public Menu()
         {
             entrada = new EntradaDados();
-            cad = new Cadastro();
-            pedido = new Pedido();
         }
 
         private void Opcoes()
         {
-            Console.WriteLine("\n1 - Inserir produto digital");
-            Console.WriteLine("\n2 - Inserir produto alimentar");
-            Console.WriteLine("\n3 - Escrever dados");
-            Console.WriteLine("\n4 - Escrever média de valores");
-            Console.WriteLine("\n5 - Aumentar valores acima da média");
-            Console.WriteLine("\n6 - Ordenar pela descrição");
-            Console.WriteLine("\n7 - Listar produtos abaixo da média");
-            Console.WriteLine("\n8 - Excluir produto");
-            Console.WriteLine("\n9 - Editar produto");
-            Console.WriteLine("\n10 - Insere item no pedido");
-            Console.WriteLine("\n11 - Lista pedido");
-            Console.WriteLine("\n12 - Deletar produto do pedido");
-            Console.WriteLine("\n13 - Alterar quantidade de produto no pedido");
-            Console.WriteLine("\n14 - Listar valor total do pedido");
-            Console.WriteLine("\n15 - Inserir data e validar");
+            Console.WriteLine("\n1 - Inserir atleta");
+            Console.WriteLine("\n2 - Excluir atleta");
+            Console.WriteLine("\n3 - Listar atletas");
+            Console.WriteLine("\n4 - Buscar atleta por nome");
+            Console.WriteLine("\n5 - Filtrar por situação e idade mínima");
+            Console.WriteLine("\n6 - Excluir por situação");
             Console.WriteLine("\n0 - Sair");
-        }
-
-        private int MostraOpcoes()
-        {
-            Opcoes();
-            return entrada.LeInteiro("\nInforme a sua opção:");
         }
 
         public void MenuOpcoes()
         {
-            int opc = MostraOpcoes();
-
-            while (opc != 0)
+            Opcoes();
+            int opc = entrada.LeInteiro("\nInforme a sua opção:");
+            switch (opc)
             {
-                switch (opc)
-                {
-                    case 0:
-                        break;
-                    case 1:
-                        LeProdutoDigital();
-                        break;
-                    case 2:
-                        LeProdutoAlimentar();
-                        break;
-                    case 3:
-                        EscreveDados();
-                        break;
-                    case 4:
-                        EscreveMedia();
-                        break;
-                    case 5:
-                        IncrementaValoresAcimaDaMedia();
-                        break;
-                    case 6:
-                        cad.OrdenaProdutosAlfabeticamente();
-                        break;
-                    case 7:
-                        EscreveProdutosAbaixoDaMedia();
-                        break;
-                    case 8:
-                        ExcluiProduto();
-                        break;
-                    case 9:
-                        EditarProduto();
-                        break;
-                    case 10:
-                        LeItemPedido();
-                        break;
-                    case 11:
-                        ImprimePedido();
-                        break;
-                    case 12:
-                        DeletarProdutoPedido();
-                        break;
-                    case 13:
-                        AlteraQuantidadeProdutoPedido();
-                        break;
-                    case 14:
-                        ValorTotalPedido();
-                        break;
-                    case 15:
-                        ArmazenaValidaData();
-                        break;
-                    default:
-                        Console.WriteLine("\nOpção Inválida");
-                        break;
-                }
-                opc = MostraOpcoes();
+                case 0:
+                    break;
+                case 1:
+                    InsereAtleta();
+                    break;
+                case 2:
+                    ExcluiAtleta(entrada.LeString("\nNome do atleta"));
+                    break;
+                case 3:
+                    ImprimeAtletas(null);
+                    break;
+                case 4:
+                    ImprimeAtletas(entrada.LeString("\nNome do atleta"));
+                    break;
+                case 5:
+                    FiltraAtletas(entrada.LeString("\nSituação do atleta"), entrada.LeInteiro("\nIdade do atleta"));
+                    break;
+                case 6:
+                    ExcluiAtleta(entrada.LeString("\nSituação do atleta"));
+                    break;
+                default:
+                    Console.WriteLine("\nOpção Inválida");
+                    break;
             }
-
-        }
-
-        private void ArmazenaValidaData()
-        {
-
-            Data d = new Data(entrada.LeInteiro("Digite o dia"),
-                entrada.LeInteiro("Digite o mês"),
-                entrada.LeInteiro("Digite o ano")
-            );
-            Boolean b = d.DataValida(d);
-            if (b)
+            if (opc != 0)
             {
-                Console.WriteLine("\n" + d + "é uma data válida");
-            }
-            else
-            {
-                Console.WriteLine("\n" + d + "é uma data inválida");
+                MenuOpcoes();
             }
         }
 
-        private void AlteraQuantidadeProdutoPedido()
+        private void InsereAtleta()
         {
-            pedido.AlterarQuantidadeProduto(entrada.LeInteiro("Digite o código do produto a ser alterado no pedido"));
-        }
-
-        private void ValorTotalPedido()
-        {
-            pedido.InformaValorTotal();
-        }
-
-        private void DeletarProdutoPedido()
-        {
-            pedido.RemoverProduto(entrada.LeInteiro("Digite o código do produto a ser retirado do pedido"));
-        }
-        private void EditarProduto()
-        {
-            cad.EditarProduto(entrada.LeInteiro("Digite o código do produto a ser editado"));
-        }
-        private void ExcluiProduto()
-        {
-            cad.ExcluiProduto(entrada.LeInteiro("Digite o código do produto a ser excluído"));
-        }
-
-        private void LeProdutoDigital()
-        {
-            ProdutoDigital pd = new ProdutoDigital
-            {
-                TamanhoArquivo = entrada.LeInteiro("Digite o tamanho do arquivo:"),
-                VersaoProduto = entrada.LeInteiro("Digite a versão do produto:"),
-                Codigo = entrada.LeInteiro("Digite o código do produto:"),
-                Descricao = entrada.LeString("Digite o nome do produto:"),
-                Valor = entrada.LeDouble("Digite o valor do produto:"),
-
-            };
-            cad.InsereDados(pd);
-        }
-
-        private void LeProdutoAlimentar()
-        {
-            ProdutoAlimentar pa = new ProdutoAlimentar
-            {
-                DataFabricacao = entrada.LeData("Digite a data de fabricação do produto:"),
-                DataValidade = entrada.LeData("Digite a data de validade do produto:"),
-                Codigo = entrada.LeInteiro("Digite o código do produto:"),
-                Descricao = entrada.LeString("Digite o nome do produto:"),
-                Valor = entrada.LeDouble("Digite o valor do produto:"),
-
-            };
-            cad.InsereDados(pa);
-        }
-
-        private void EscreveDados()
-        {
-            for (int i = 0; i < cad.Tamanho(); i++)
-            {
-                Console.WriteLine("\n" + cad.GetProduto(i).ToString());
-            }
 
         }
 
-        private void EscreveMedia()
+        private void ExcluiAtleta(string filtro)
         {
-            if (cad.Tamanho() <= 0)
-            {
-                Console.WriteLine("\nDeve existir valores para ser gerada uma média");
-                return;
-            }
-            Console.WriteLine("\nA média de valor dos produtos é: " + MediaValoresProdutos());
         }
 
-        private double MediaValoresProdutos()
+        private void ImprimeAtletas(string nome)
         {
-            int tam = cad.Tamanho();
-            double total = 0;
-            for (int i = 0; i < tam; i++)
-            {
-                total += cad.GetProduto(i).Valor;
-            }
-            media = total / tam;
-            return media;
         }
 
-        private void IncrementaValoresAcimaDaMedia()
+        private void FiltraAtletas(string situacao, int idade)
         {
-            MediaValoresProdutos();
-            double porcentagem = entrada.LeDouble("Digite a porcentagem para reajuste: ");
-            for (int i = 0; i < cad.Tamanho(); i++)
-            {
-                Produto p = cad.GetProduto(i);
-                if (p.Valor >= media)
-                {
-                    p.Reajuste(OperacaoEnum.Adicionar, porcentagem);
-                }
-            }
-        }
 
-        private void EscreveProdutosAbaixoDaMedia()
-        {
-            MediaValoresProdutos();
-            for (int i = 0; i < cad.Tamanho(); i++)
-            {
-                Produto p = cad.GetProduto(i);
-                if (p.Valor < media)
-                {
-                    Console.WriteLine("\n" + p.ToString());
-                }
-            }
-
-        }
-
-        private void LeItemPedido()
-        {
-            int codigo = entrada.LeInteiro("Informe o código do produto:");
-            Produto p = cad.GetProdutoByCodigo(codigo);
-            int qtde = 0;
-
-            if (p == null)
-            {
-                Console.WriteLine("\nCódigo não encontrado no cadastro de produtos");
-            }
-            else
-            {
-                qtde = entrada.LeInteiro("Informe a quantidade itens:");
-                try
-                {
-                    itemPedido = new ItemPedido(p, qtde);
-                    pedido.InsereItem(itemPedido);
-                } catch (Exception ex) {
-                    Console.WriteLine("\n" + ex.Message);
-                }
-               
-            }
-        }
-        private void ImprimePedido()
-        {
-            for (int i = 0; i < pedido.Tamanho(); i++)
-            {
-                Console.WriteLine(pedido.GetItemPedido(i).ToString());
-            }
         }
     }
 }
